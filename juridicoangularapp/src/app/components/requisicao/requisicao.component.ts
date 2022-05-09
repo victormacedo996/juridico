3087ec10793386cd0b7b6786fdbd121c9dcc93ef
Diogo Feital <diiogofeital@gmail.com>
2022-05-08 21:53:58 -0300

    Retirando debbuger, ajustando coluna front.

--- juridicoangularapp/src/app/components/requisicao/requisicao.component.ts
+++ juridicoangularapp/src/app/components/requisicao/requisicao.component.ts
@@ -1,4 +1,4 @@
-/* Componente responsavel por criar uma requisicao de análise de solicitacao 
+/* Componente responsavel por criar uma requisicao de análise de solicitacao
    Esse componente terá subcomponentes que serão modais
    Essas modais irão chamar um serviço que enviará os dados para o back-end
    A controller que receberá esses dados no back-end será a AnaliseJuridicoController.java
@@ -21,7 +21,6 @@ export class RequisicaoComponent implements OnInit {
 
   teste(){
     this.carregarParametros.carregarParametrosRequisicao().subscribe(response => {
-      debugger;
       console.log(response);
     },error=> {
 
@@ -35,5 +34,5 @@ export class RequisicaoComponent implements OnInit {
   fecharModal(event:boolean){
     this.showModal = event;
   }
-  
+
 }
