58e93e49a595d6bb26e324707f741cf5041ee629
jvillavicenciot <ads@senacrio.com.br>
2022-05-24 18:36:27 -0300

    Atualização do relacionamento AtendiementoRegistroEnvolvido

--- src/main/java/br/senac/juridico/model/AtendimentoRegistroEnvolvido.java
+++ src/main/java/br/senac/juridico/model/AtendimentoRegistroEnvolvido.java
@@ -3,11 +3,16 @@ package br.senac.juridico.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "atendimento_registro_envolvido")
 public class AtendimentoRegistroEnvolvido implements Serializable{
 
 	/**
@@ -30,8 +35,8 @@ public class AtendimentoRegistroEnvolvido implements Serializable{
 	private int status;
 
 	//@ManyToMany????????????
-	@OneToMany
-	@Column(name = "atendimento_registro_id")
+	@ManyToOne
+	@JoinColumn(name = "atendimento_registro_id")
 	private AtendimentoRegistro atendimentoRegistro;
 
 	public Integer getId() {
